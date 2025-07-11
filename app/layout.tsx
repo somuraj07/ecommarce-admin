08e81f6c38ab8ae04b9f32749dd4c38269f3ae06
somuraj07 <somasankar33@gmail.com>
2025-07-11 17:28:47 +0530

    added neon database

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-Provider";
 import "./globals.css";
+import prismadb from "@/lib/prismadb";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <ClerkProvider>
        <html lang="en">
